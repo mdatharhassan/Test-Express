fac4c3388d8cf2dcbf5984b2516683a71d789235
athar <iitropar109@gmail.com>
2025-12-10 20:40:03 +0530

    forth  version

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/mongoDB";
 const app = express();
+
+// Cors configuration
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://hotel-booking-management-system-fro.vercel.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+};
+
+// Middleware to parse JSON requests
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
