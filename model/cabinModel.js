92a2301474c60d46eef4873eaa2be0cda66966a9
Md Athar Hassan <70963266+mdatharhassan@users.noreply.github.com>
2025-12-12 00:16:40 +0530

    Change cabin name field to allow non-unique values

--- model/cabinModel.js
+++ model/cabinModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const cabinSchema = new Schema({
-  name: { type: String, unique: true, required: [true, "Name is required"] },
+  name: { type: String, unique: false, required: [true, "Name is required"] },
   maxCapacity: { type: Number, required: true },
   regularPrice: { type: Number, required: true },
   discount: { type: Number, required: true },
